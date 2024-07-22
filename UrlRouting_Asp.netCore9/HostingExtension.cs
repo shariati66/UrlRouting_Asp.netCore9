8cae50b509f550b71c0c3368fa16baedc6251ccd
shariati66 <shariati1987@gmail.com>
2024-07-22 07:58:25 +0330

    Add Hosting Class to separate middleware and etc from program.cs file

---
+++ UrlRouting_Asp.netCore9/HostingExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace UrlRouting_Asp.netCore9
+{
+    public static class HostingExtension
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            return builder.Build();
+        }
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            return app;
+        }
+    }
+}
--- UrlRouting_Asp.netCore9/Program.cs
+++ UrlRouting_Asp.netCore9/Program.cs
@@ -1,5 +1,8 @@
+using UrlRouting_Asp.netCore9;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//var app = builder.Build();
+var app = builder.ConfigureServices().ConfigurePipeline();
 
 app.MapGet("/", () => "Hello World!");
 
